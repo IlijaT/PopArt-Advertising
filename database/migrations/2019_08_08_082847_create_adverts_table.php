960fb79523bd7e372acd322cc117af41788defa3
IlijaT <tatalovicilij@gmail.com>
2019-08-08 10:39:10 +0200

    Added Advert model and migrations

---
+++ database/migrations/2019_08_08_082847_create_adverts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('state');
+            $table->integer('price');
+            $table->string('location');
+            $table->string('phone', 40);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
