621d3f51edca2a03e8f842e217eda9e47fce971f
IlijaT <tatalovicilija@gmail.com>
2019-08-08 13:10:18 +0200

    Logout funcionality

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,8 +10,23 @@
     @if(Auth::check())
 
     <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="mr-2">{{ Auth::user()->name }}</a>
-        <a class="btn btn-sm btn-outline-secondary" href="/logout">Logout</a>
+      <li style="list-style: none;" class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
+
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+          </div>
+      </li>
     </div>
 
     @else
