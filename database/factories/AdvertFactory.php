bd47d3104c7cfd046c6ca752fce9589dd71dc8fe
IlijaT <tatalovicilij@gmail.com>
2019-08-08 11:30:43 +0200

    Added Advert and Category model factories

---
+++ database/factories/AdvertFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Advert;
+use Faker\Generator as Faker;
+
+$factory->define(Advert::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
+        'description' => $faker->sentence,
+        'state' => $faker->word,
+        'price' => $faker->numberBetween(100, 100000),
+        'location' => $faker->address,
+        'phone' => $faker->phoneNumber,
+        'user_id' => factory(App\User::class)->create()->id,
+        'category_id' => factory(App\Category::class)->create()->id
+    ];
+});
