ac39fbdee0b30e5141ff9edccc0c43075967841c
IlijaT <tatalovicilija@gmail.com>
2019-08-10 12:24:29 +0200

    Categories displayed in sider

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,35 +1,21 @@
 <aside class="col-md-4 popart-sidebar">
   <div class="p-3 mb-3 bg-light rounded">
-    <h4 class="font-italic">Tags</h4>
-    <ol class="list-unstyled mb-0">
-
-      {{-- @foreach($tags as $tag)
-
-      <li>
-        <a href="/posts/tags/{{$tag}}">
-          {{ $tag }}
-        </a>
-      </li>
-
-      @endforeach --}}
-
-    </ol>
-  </div>
-
-  <div class="p-3">
     <h4 class="font-italic">Categories</h4>
     <ol class="list-unstyled mb-0">
 
-      {{-- @foreach($archives as $stats)
+      @foreach($categories as $category)
 
-       <li>
-        <a href="/?month={{ $stats['month'] }}&year={{ $stats['year'] }}">
-          {{ $stats['month'] . ' ' . $stats['year'] }}
-        </a>
+      <li>
+        @if($category->parent_id === null)
+          <a href="/categories/{{ $category->id }}">{{ $category->name }}</a>
+          @if(count($category->subcategory))
+            @include('layouts.subCategoryList',['subcategories' => $category->subcategory])
+          @endif 
+        @endif
       </li>
 
-      @endforeach --}}
-     
+      @endforeach
+
     </ol>
   </div>
 
