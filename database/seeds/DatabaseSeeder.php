380de0a2c4d9142832837bd7655df3fd8be81a4a
IlijaT <tatalovicilija@gmail.com>
2019-08-10 11:47:40 +0200

    DB seeds populated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
+use App\Advert;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $racunari = factory(Category::class)->create(['name' => 'Racunari']);
+        $automobili = factory(Category::class)->create(['name' => 'Automobili']);
+        factory(Category::class)->create(['name' => 'Telefoni']);
+        factory(Category::class)->create(['name' => 'Bicikli']);
+
+        $komponente = factory(Category::class)->create([
+            'name' => 'Komponente',
+            'parent_id' => $racunari->id
+        ]);
+
+        factory(Category::class)->create([
+            'name' => 'Desktop',
+            'parent_id' => $racunari->id
+        ]);
+
+        factory(Category::class)->create([
+            'name' => 'Laptop',
+            'parent_id' => $racunari->id
+        ]);
+
+        $grafickeKartice = factory(Category::class)->create([
+            'name' => 'Graficke kartice',
+            'parent_id' => $komponente->id
+        ]);
+
+        factory(Category::class)->create([
+            'name' => 'Delovi',
+            'parent_id' => $automobili->id
+        ]);
+
+        factory(Category::class)->create([
+            'name' => 'Oprema',
+            'parent_id' => $automobili->id
+        ]);
+
+        $john = factory(User::class)->create(['name' => 'John Doe', 'email' => 'john@example.com']);
+
+        factory(Advert::class, 5)->create([
+            'category_id' => $grafickeKartice->id,
+            'user_id' => $john->id
+        ]);
+
+        factory(Advert::class, 10)->create([
+            'category_id' => $automobili->id,
+            'user_id' => $john->id
+        ]);
+
+        factory(Advert::class, 5)->create([
+            'category_id' => $racunari->id,
+            'user_id' => $john->id
+        ]);
     }
 }
